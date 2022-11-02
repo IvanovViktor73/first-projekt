45f809e2cce71a5f0c2878a5a9fe60f955883d2b
viktar <v-a-i@mail.ru>
2022-11-02 19:45:19 +0100

    ...

---
+++ src/DoubleLesson.java
@@ -0,0 +1,37 @@
+public class DoubleLesson {
+    public static void main(String[] args) {
+        int rabbits = 5;
+        long time = 456_456_456_456L;
+
+
+        // дробные числа
+        float firstNumber; // 4 байт
+        double secondNumber; // 8 байт
+
+        double number = 5.6;
+        int n = 5;
+        System.out.println(number = number + 2);
+        number++;
+        number += 4.1;
+        System.out.println(number);
+
+
+        float f = 1.0F;
+
+
+        //символьнфй тип данных
+        char s; // 2 байта
+        char symbol = 'a'+ 1;
+        System.out.println(symbol);
+
+
+
+
+
+
+
+
+
+
+    }
+}
